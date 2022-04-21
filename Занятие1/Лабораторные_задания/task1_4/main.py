ab03577e83a730bf308b9b3f2789e17bc5bb148f
ValeryKharkov <kharkov.valery@gmail.com>
2022-04-21 21:28:49 +0300

    Практика от 21.04.22

--- Занятие1/Лабораторные_задания/task1_4/main.py
+++ Занятие1/Лабораторные_задания/task1_4/main.py
@@ -1,9 +1,9 @@
 def list_comprehension(words: list) -> list:
-    return [...]  # TODO
+    return [i.capitalize() for i in words]  #
 
 
 def list_map(words: list) -> list:
-    return list(...)  # TODO
+    return list(map(str.capitalize, words))  #
 
 
 def task():
--- Занятие1/Лабораторные_задания/task4_1/main.py
+++ Занятие1/Лабораторные_задания/task4_1/main.py
@@ -4,7 +4,8 @@ from itertools import count
 def task():
     counter = count(100, 10)
 
-    # TODO распечатать с столбик первые 10 чисел бесконечного итератора
+    for i in range(10):
+        print(next(counter))# распечатать с столбик первые 10 чисел бесконечного итератора
 
 
 if __name__ == "__main__":
